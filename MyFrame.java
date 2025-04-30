366871a25053bb0145e7f905f8360bff1ff5c975
ibrahim483 <ibrahim.abbalta@gmail.com>
2025-05-01 01:55:32 +0200

    Initial commit

---
+++ MyFrame.java
@@ -0,0 +1,17 @@
+public class MyFrame {
+
+    public static void main(String[] args) {
+        // Create a new JFrame
+        javax.swing.JFrame frame = new javax.swing.JFrame("My Frame");
+        
+        // Set the default close operation
+        frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        
+        // Set the size of the frame
+        frame.setSize(400, 300);
+        
+        // Make the frame visible
+        frame.setVisible(true);
+
+    }
+} 
