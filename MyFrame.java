90d0ae9fdd8930ec9176bb7c8760a24133d3708c
ibrahim483 <ibrahim.abbalta@gmail.com>
2025-05-01 11:03:22 +0200

    add dance with me label

--- MyFrame.java
+++ MyFrame.java
@@ -15,7 +15,8 @@ public class MyFrame {
         // Make the frame visible
         frame.setVisible(true);
 
-        JLabel label = new JLabel("Hello, World!");
+        JLabel label = new JLabel("dance with me", JLabel.CENTER);
+        label.setFont(new java.awt.Font("Arial", java.awt.Font.PLAIN, 24)); // Set font and size
         label.setHorizontalAlignment(JLabel.CENTER);
         label.setVerticalAlignment(JLabel.CENTER);
         label.setSize(200, 100);
